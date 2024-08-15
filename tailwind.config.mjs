3304038c25daf8fc03eb1b6a46f7d2bf4f7f46f6
Nikos Tsilas <tsilasnikos@gmail.com>
2024-08-15 15:54:57 +0300

    Completed the FAQ element

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'really-light-blue': '#D3E4FF',
+				'light-blue': '#9AC3FF',
+				'dark-gray': '#2f2f2f',
+				'grayish-blue': '#a6b3c9',
+			  },
+			  spacing: {
+				'85vw': '85vw',
+			  }
+		},
 	},
 	plugins: [],
 }
