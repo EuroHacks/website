32325cbdb02c1767c696ec6a30cfcae538eeaea2
Nikos Tsilas <tsilasnikos@gmail.com>
2024-08-16 13:47:21 +0300

    Tailwindify the navbar and the about us page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,11 +6,23 @@ export default {
 			colors: {
 				'really-light-blue': '#D3E4FF',
 				'light-blue': '#9AC3FF',
+				'pale-sky-blue': '#99C2FF',
 				'dark-gray': '#2f2f2f',
 				'grayish-blue': '#a6b3c9',
 				'background': '#263954;',
+				'soft-peach': '#FFF2DE',
+				'steel-blue': '#2E548E',
+				'midnight-slate': '#39537A',
+				'frosted-blue': '#76ADFF'
+			  },
+			  width: {
+				'24vw': '24vw',
+			  },
+			  height: {
+				'18vw': '18vw'
 			  },
 			  spacing: {
+				'0.4vw': '0.4vw',
 				'85vw': '85vw',
 			  }
 		},
