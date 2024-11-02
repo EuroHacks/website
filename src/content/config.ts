11bf8651cd5fe08a466ed0cb9f61cb141cf13476
Nikos Tsilas <tsilasnikos@gmail.com>
2024-11-02 13:52:21 +0200

    Make a FAQ list

--- src/content/config.ts
+++ src/content/config.ts
@@ -72,7 +72,20 @@ const eventCollection = defineCollection({
     })
 })
 
+const faqCollection = defineCollection({
+    type: "data",
+    schema: z.object({
+        questions: z.array(z.object(
+            {
+                question: z.string(),
+                answer: z.string()
+            }
+        ))
+    })
+});
+
 export const collections = {
     'organizers': organizerCollection,
-    'events': eventCollection
+    'events': eventCollection,
+    'faq': faqCollection
 };
