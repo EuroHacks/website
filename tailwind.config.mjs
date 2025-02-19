f0b89e7d07f81fe89fcc767d906268a3fd90e8c3
maxc0des <schneider.max16@yahoo.com>
2025-02-19 20:23:20 +0100

    made the site mobile friendly

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
 			screens: {
 				sm: '0px',
 				md: '640px',
-				lg: '1024px',
+				lg: '1025px',
 			},
 			colors: {
 				'bg': 					"#F9F0DD",
@@ -36,12 +36,14 @@ export default {
 				'90vw': '90vw',
 				'100vw': '100vw',
 				'event_cards': '22vw',
+				'event_cards_mobile': '60vw',
 			  },
 			  height: {
 				'160px': '160px',
 				'18vw': '18vw',
 				'88vh': '88vh',
 				'event_cards': '22vw',
+				'event_cards_mobile': '60vw',
 			  },
 			  spacing: {
 				'-20px': '-20px',
